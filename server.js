f0875efa61e7591329cc202f4a8e8f5c4734762a
Lalit Kapoor <lalitkapoor@gmail.com>
2015-06-16 09:58:40 -0500

    add express

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var http = require('http')
+var express = require('express')
 var serveStatic = require('serve-static')
 var finalhandler = require('finalhandler')
 
 var port = process.env.PORT || 3000
 
-var serve = serveStatic('public', {'index': ['index.html']})
+var static = serveStatic('public', {'index': ['index.html']})
 
-var server = http.createServer(function(req, res) {
-  var done = finalhandler(req, res)
-  serve(req, res, done)
-})
+var app = express()
+app.use(static)
+
+var server = http.createServer(app)
 
 server.listen(port, function(error) {
   if (error) return console.error(error)
